35e9bf70ec8619d5b6792cec01a88979d5fdec25
Ukasquared <ukachukwujudith95@gmail.com>
2023-11-20 09:50:14 +0100

    bubble-sort"

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		state = false;
 		/* swap value inside inner loop*/
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
